25ad8b9194a73148c502d6aba7d627f114109c03
Nícolas Paulo Nasário <nicolaspn09@gmail.com>
2026-07-11 19:14:00 -0300

    Adiciona clique fisico no Cloudflare para evitar timeout em VPS

--- navegador.py
+++ navegador.py
@@ -95,6 +95,17 @@ class NavegadorPy:
             iframe_cloudflare = WebDriverWait(self.navegador, timeout_captcha).until(
                 EC.presence_of_element_located((By.XPATH, "//iframe[contains(@src, 'cloudflare')]"))
             )
+            
+            # 1.5 TENTA CLICAR NO IFRAME
+            # Em servidores (VPS), o Cloudflare raramente valida passivamente. Ele exige um clique.
+            try:
+                time.sleep(1)
+                ActionChains(self.navegador).move_to_element(iframe_cloudflare).click().perform()
+                print("[INFO] Clique físico enviado ao Cloudflare.")
+                time.sleep(2)
+            except Exception as e:
+                print(f"[Aviso] Falha ao tentar clicar no Cloudflare: {e}")
+                
             # Alterna o foco do driver para dentro do iframe
             self.navegador.switch_to.frame(iframe_cloudflare)
         except Exception:
